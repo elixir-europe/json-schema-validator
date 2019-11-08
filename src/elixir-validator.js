05b258092d21bcb1f428c16f249b5af673b091a0
Rolando Fernandez <rolando@ebi.ac.uk>
2019-11-08 11:34:01 +0000

    restructured ElixirValidator

--- src/elixir-validator.js
+++ src/elixir-validator.js
@@ -1,153 +1,74 @@
 /**
  * Created by rolando on 08/08/2018.
  */
+
 const Promise = require('bluebird');
-const R = require('rambda');
-const logger = require("./winston");
-const curies = require ("./utils/curie_expansion");
 const path = require("path");
 const fs = require('fs');
-const md5 = require('md5');
-
-const ErrorReport = require('./model/error-report');
-const ValidationReport = require('./model/validation-report');
-
-let Ajv = require("ajv");
+const ajv = require("ajv");
 const request = require("request-promise");
-const cachedSchemas = {};
-
+const AppError = require("./model/application-error");
 
 /**
  *
  * Wraps the generic validator, outputs errors in custom format.
  *
  */
+
 class ElixirValidator {
-    constructor(keywords, options) {
-        this.schemaCache = {};
+    constructor(customKeywordValidators, baseSchemaPath){
         this.validatorCache = {};
-        this.baseSchemaPath = null;
-
-        if (options) {
-            options['allErrors'] = true;
-            options['schemaId'] = 'auto';
-            if (!options.loadSchema) {
-                options.loadSchema = this.loadSchemaRef;
-            }
-
-            if (options.baseSchemaPath) {
-                this.baseSchemaPath = options.baseSchemaPath
-            }
-
-        } else {
-            options = {allErrors: true, schemaId: 'auto', loadSchema: this.loadSchemaRef};
-        }
-
-
-        this.ajv = new Ajv(options);
-        this.ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
-
-        if (Array.isArray(keywords)) {
-            for (let keyword of keywords) {
-                new keyword(this.ajv, options)
-            }
-        }
+        this.cachedSchemas = {};
+        this.ajvInstance = this.constructAjv(customKeywordValidators);
+        this.baseSchemaPath = baseSchemaPath;
+        this.customKeywordValidators = customKeywordValidators;
     }
 
-    loadSchemaRef(uri) {
-
-
-        if(cachedSchemas[uri]) {
-            return Promise.resolve(cachedSchemas[uri]);
-        } else {
-            if (this.baseSchemaPath) {
-                let ref = path.join(this.baseSchemaPath, uri);
-                console.log('loading ref ' + ref);
-                let jsonSchema = fs.readFileSync(ref);
-                let loadedSchema = JSON.parse(jsonSchema);
-                loadedSchema["$async"] = true;
-                cachedSchemas[uri] = loadedSchema;
-                return Promise.resolve(loadedSchema);
-            }
-            else {
-                return new Promise((resolve, reject) => {
-                    request({
-                        method: "GET",
-                        url: uri,
-                        json: true
-                    }).then(resp => {
-                        const loadedSchema = resp;
-                        loadedSchema["$async"] = true;
-                        cachedSchemas[uri] = loadedSchema;
-                        resolve(loadedSchema);
-                    }).catch(err => {
-                        reject(err);
-                    });
-                });
-            }
-        }
-    }
-
-    validate_with_remote_schema(schemaUri, document) {
-        return this.getSchema(schemaUri)
-            .then(schema => {return this.validate(document, schema)})
-    }
-
-    validate(schema, document) {
-        return this._validateSingleSchema(schema, document)
-            .then(valErrors => {return this.parseValidationErrors(valErrors)})
-            .then(parsedErrors => {return this.generateValidationReport(parsedErrors)})
-    }
-
-    _validateSingleSchema (inputSchema, inputObject) {
+    validateSingleSchema(inputSchema, inputObject) {
         inputSchema["$async"] = true;
-        const schemaId = !inputSchema['$id'] ? md5(JSON.stringify(inputSchema)) : inputSchema['$id'];
+        const schemaId = inputSchema['$id'];
 
-        logger.log("silly", "Running validation...");
         return new Promise((resolve, reject) => {
-
-            let compiledSchemaPromise = null;
-            if(this.validatorCache[schemaId]) {
-                compiledSchemaPromise = Promise.resolve(this.validatorCache[schemaId]);
-                console.info()
-            } else {
-                compiledSchemaPromise = this.ajv.compileAsync(inputSchema);
-            }
+            const compiledSchemaPromise = this.getValidationFunction(inputSchema);
 
             compiledSchemaPromise.then((validate) => {
                 this.validatorCache[schemaId] = validate;
                 Promise.resolve(validate(inputObject))
                     .then((data) => {
                             if (validate.errors) {
-                                logger.log("debug", this.ajv.errorsText(validate.errors, {dataVar: inputObject.alias}));
                                 resolve(validate.errors);
                             } else {
                                 resolve([]);
                             }
                         }
-                    ).catch((err, errors) => {
-                    if (!(err instanceof Ajv.ValidationError)) {
-                        logger.log("error", "An error ocurred while running the validation.");
-                        reject(new AppError("An error ocurred while running the validation."));
+                    ).catch((err) => {
+                    if (!(err instanceof ajv.ValidationError)) {
+                        console.error("An error occurred while running the validation.");
+                        reject(new AppError("An error occurred while running the validation."));
                     } else {
-                        logger.log("debug", this.ajv.errorsText(err.errors, {dataVar: inputObject.alias}));
+                        console.debug("debug", this.ajvInstance.errorsText(err.errors, {dataVar: inputObject.alias}));
                         resolve(err.errors);
                     }
                 });
             }).catch((err) => {
-                console.log("async schema compiled encountered and error");
-                console.log(err.stack);
+                console.error("async schema compiled encountered and error");
+                console.error(err.stack);
                 reject(err);
             });
         });
     }
 
+    validateWithRemoteSchema(schemaUri, document) {
+        return this.getSchema(schemaUri)
+            .then(schema => {return this.validateSingleSchema(document, schema)})
+    }
+
     getSchema(schemaUri) {
-        if(! cachedSchemas[schemaUri]) {
+        if(! this.cachedSchemas[schemaUri]) {
             return new Promise((resolve, reject) => {
-                this.fetchSchema(schemaUri)
+                ElixirValidator.fetchSchema(schemaUri)
                     .then(schema => {
-                        cachedSchemas[schemaUri] = schema;
+                        this.cachedSchemas[schemaUri] = schema;
                         resolve(schema);
                     })
                     .catch(err => {
@@ -155,11 +76,11 @@ class ElixirValidator {
                     })
             });
         } else {
-            return Promise.resolve(cachedSchemas[schemaUri]);
+            return Promise.resolve(this.cachedSchemas[schemaUri]);
         }
     }
 
-    fetchSchema(schemaUrl) {
+    static fetchSchema(schemaUrl) {
         return request({
             method: "GET",
             url: schemaUrl,
@@ -167,34 +88,74 @@ class ElixirValidator {
         });
     }
 
-    insertSchemaId(schema) {
-        if(schema["id"]) {
-            schema["$id"] = schema["id"];
-        }
-        return Promise.resolve(schema);
+    getValidationFunction(inputSchema) {
+        return ElixirValidator._getValidationFunction(inputSchema, this.validatorCache, this.ajvInstance);
     }
 
-    /**
-     * Ingest error reports from ajvError objects
-     * @param errors
-     */
-    parseValidationErrors(errors){
-        return Promise.resolve(R.map(ajvErr => new ErrorReport(ajvErr), errors));
-    }
+    static _getValidationFunction(inputSchema, validatorCache , ajv) {
+        const schemaId = inputSchema['$id'];
 
-    generateValidationReport(errors) {
-        let report = null;
-
-        if(errors.length > 0) {
-            report = new ValidationReport("INVALID", errors);
+        if(validatorCache[schemaId]) {
+            return Promise.resolve(validatorCache[schemaId]);
         } else {
-            report =  ValidationReport.okReport();
+            return Promise.resolve(ajv.compileAsync(inputSchema));
         }
-
-        return Promise.resolve(report);
     }
 
+    constructAjv(customKeywordValidators) {
+        const ajvInstance = new ajv({allErrors: true, schemaId: 'id', loadSchema: this.generateLoadSchemaRefFn()});
+        ajvInstance.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
+        ElixirValidator._addCustomKeywordValidators(ajvInstance, customKeywordValidators);
 
+        return ajvInstance
+    }
+
+    static _addCustomKeywordValidators(ajvInstance, customKeywordValidators) {
+        customKeywordValidators.forEach(customKeywordValidator => {
+            ajvInstance = customKeywordValidator.configure(ajvInstance);
+        });
+
+        return ajvInstance;
+    }
+
+    generateLoadSchemaRefFn() {
+        const cachedSchemas = this.cachedSchemas;
+        const baseSchemaPath = this.baseSchemaPath;
+
+        const loadSchemaRefFn = (uri) => {
+            if(cachedSchemas[uri]) {
+                return Promise.resolve(cachedSchemas[uri]);
+            } else {
+                if (baseSchemaPath) {
+                    let ref = path.join(baseSchemaPath, uri);
+                    console.log('loading ref ' + ref);
+                    let jsonSchema = fs.readFileSync(ref);
+                    let loadedSchema = JSON.parse(jsonSchema);
+                    loadedSchema["$async"] = true;
+                    cachedSchemas[uri] = loadedSchema;
+                    return Promise.resolve(loadedSchema);
+                }
+                else {
+                    return new Promise((resolve, reject) => {
+                        request({
+                            method: "GET",
+                            url: uri,
+                            json: true
+                        }).then(resp => {
+                            const loadedSchema = resp;
+                            loadedSchema["$async"] = true;
+                            cachedSchemas[uri] = loadedSchema;
+                            resolve(loadedSchema);
+                        }).catch(err => {
+                            reject(err);
+                        });
+                    });
+                }
+            }
+        };
+
+        return loadSchemaRefFn;
+    }
 }
 
 module.exports = ElixirValidator;
