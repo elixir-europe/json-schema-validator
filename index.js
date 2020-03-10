359d578805c0dd9ba5eb656a341e77560fa83b44
Karoly Erdos <karoly@ebi.ac.uk>
2020-03-10 12:03:24 +0000

    Expose new keyword: isValidTaxonomy

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ let ElixirValidator  = require('./src/elixir-validator');
 let GraphRestriction = require('./src/keywords/graph_restriction');
 let isChildTermOf = require('./src/keywords/ischildtermof');
 let isValidTerm = require('./src/keywords/isvalidterm');
+let isValidTaxonomy = require('./src/keywords/isvalidtaxonomy');
 
 module.exports = {
     ElixirValidator,
     GraphRestriction,
     isChildTermOf,
-    isValidTerm
+    isValidTerm,
+    isValidTaxonomy
 };
